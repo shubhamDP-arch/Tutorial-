be99d2e8a46670a1aa6dfa208bb649323a7084cf
backend <shubhamprabhu1001@gmail.com>
2024-08-11 16:12:54 +0530

    basic router

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import {app} from './app.js'
 
 //configs
-
-app = express()
-
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}))
 
 dotenv.config({
   path:'.env'
 })
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true, limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+
 connectDB()
 .then(()=>{
   app.listen(process.env.PORT || 8000, ()=>
